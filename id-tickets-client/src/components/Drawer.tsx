18005119959f2b5e698ff2ed454073f74de09c28
ridwankabeer435 <ridwankabeer@gmail.com>
2023-03-14 00:07:26 -0300

    Added side drawer component

--- id-tickets-client/src/components/Drawer.tsx
+++ id-tickets-client/src/components/Drawer.tsx
@@ -1,3 +1,51 @@
-import Toolbar from '@mui/material/Toolbar';
+
+import { Divider, IconButton, Link, List, ListItem, SwipeableDrawer } from '@mui/material';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { FC, useState } from 'react';
 
 
+interface DrawerProps {
+    isOpen: boolean;
+  }
+  
+const Drawer: FC<DrawerProps> = ({ isOpen }) => {
+    const [open, setOpen] = useState(false);
+
+    return (
+    <SwipeableDrawer
+        variant="temporary"
+        anchor="left"
+        open={isOpen}
+        onOpen = {() => setOpen(true)}
+        onClose = {() => setOpen(false)}
+      >
+        <div>
+            <IconButton>
+                <ChevronLeftIcon />
+            </IconButton>
+        </div>
+        <Divider />
+   
+        <List>
+        {['Home', 'All tickets'].map((text) => (
+            <ListItem>
+            <Link 
+                color="inherit"
+                underline="none"
+                variant="button"
+                href="#"
+            >
+            {text}
+            </Link >
+            </ListItem>
+           
+          ))}
+        </List>
+          
+     
+      </SwipeableDrawer>
+
+    )
+}
+  
+export default Drawer;
